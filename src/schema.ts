4f228e8fff0f76987d039b930b0d7b4519b6b046
mbm_1607 <muhammadkhan1607@gmail.com>
2023-10-04 23:29:45 +0500

    :zap: Make email unique

--- src/schema.ts
+++ src/schema.ts
@@ -5,7 +5,7 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 export const users = sqliteTable("users", {
 	id: integer("id").primaryKey(),
 	name: text("name").notNull(),
-	email: text("email").notNull(),
+	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
 	createdAt: integer("created_at", { mode: "timestamp" }).default(
 		sql`(strftime('%s', 'now'))`,
